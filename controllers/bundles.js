3fe08dc4d4bed0a74995719c991060b165ddb325
richmark <richmark.jinn.ravina@gmail.com>
2020-01-24 20:36:28 +0800

    feat: working update bundle js without validation

--- controllers/bundles.js
+++ controllers/bundles.js
@@ -8,6 +8,7 @@
 
 const oBundleModel = require('../models/bundles');
 const { errorHandler } = require('../helpers/dbErrorHandler');
+const { _ } = require('lodash');
 
 this.setRequestBodyImage = oRequest => {
   if (typeof oRequest.files !== "undefined") {
@@ -25,15 +26,15 @@ this.setRequestBodyImage = oRequest => {
 exports.createBundle = (oRequest, oResponse) => {
   oRequest = this.setRequestBodyImage(oRequest);
   oRequest.body.products = JSON.parse(oRequest.body.products);
-  const oBundle = new oBundleModel(oRequest.body);
-  oBundle.save((oError, oData) => {
-    if (oError) {
-      return oResponse.status(400).json({
-        error: errorHandler(oError)
-      });
-    }
-    oResponse.json({ data: oData });
-  });
+  // const oBundle = new oBundleModel(oRequest.body);
+  // oBundle.save((oError, oData) => {
+  //   if (oError) {
+  //     return oResponse.status(400).json({
+  //       error: errorHandler(oError)
+  //     });
+  //   }
+  //   oResponse.json({ data: oData });
+  // });
 };
 
 /**
@@ -59,4 +60,52 @@ exports.listBundles = (oRequest, oResponse) => {
       }
       oResponse.json({ data: oData });
     });
+};
+
+/**
+ * Get Bundle by id and returns populated product with selected fields only
+ */
+exports.bundleById = (oRequest, oResponse, oNext, sId) => {
+  oBundleModel
+  .findById(sId)
+  .populate('products.product', '_id product_name price image ')
+  .exec((oError, oBundleData) => {
+    if (oError || !oBundleData) {
+      return oResponse.status(400).json({
+        error: "Bundle not found"
+      });
+    }
+    oRequest.bundle = oBundleData;
+    oNext();
+  });
+};
+
+/**
+ * returns the bundle return by bundleById function
+ */
+exports.getBundleById = (oRequest, oResponse) => {
+  return oResponse.json({ data: oRequest.bundle });
+}
+
+/**
+ * createBundle function
+ * this function create bundle
+ */
+exports.updateBundle = (oRequest, oResponse) => {
+  oRequest = this.setRequestBodyImage(oRequest);
+  oRequest.body.products = JSON.parse(oRequest.body.products);
+  _.assignIn(oRequest.bundle, oRequest.body);
+  oBundleModel.findOneAndUpdate(
+    { _id: oRequest.bundle._id },
+    { $set: oRequest.bundle },
+    { new: true },
+    (oError, oData) => {
+      if (oError) {
+        return oResponse.status(400).json({
+          error: errorHandler(oError)
+        });
+      }
+      oResponse.json({ data: oData, msg: 'Bundle updated successfully' });
+    }
+  );
 };
